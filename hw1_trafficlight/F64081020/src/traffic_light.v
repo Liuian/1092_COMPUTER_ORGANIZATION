fc5654da3a6f8ae4a7aaa5b24ac96ff5e8020a05
Liuian <liuperong@gmail.com>
2021-04-09 08:38:37 +0800

    add hw2 file

--- hw1_trafficlight/F64081020/src/traffic_light.v
+++ hw1_trafficlight/F64081020/src/traffic_light.v
@@ -28,29 +28,28 @@ module Traffic_control(
 	input pass,
 	input rst,
 	input clk,
-	input clock_counter,
+	input [11:0]clock_counter,
 	output reg R = 1'b0,
-	output reg G = 1'b0,
+	output reg G = 1'b1,
 	output reg Y = 1'b0
 );
-	reg [1:0] current_state = 2'b00;
+	reg [1:0] current_state = 2'b01;
 	reg [1:0] next_state = 2'b00;
 	parameter[1:0] red_light = 2'b00, green_light = 2'b01, yellow_light = 2'b10, none_light = 2'b11;
 	//state register
-	always @(posedge clk or posedge rst == 1)
+	always @(posedge clk or posedge rst)
 	begin
 		if(rst)                                     //rst就回第0個cycle
 			current_state = green_light;
-		else
-		begin
-			if(pass == 1 && clock_counter > 1023)  //clock counter大於1023（不是起始綠燈）且 pass = 1 時，換成起始綠燈第一個cycle
+		else if(pass == 1 && clock_counter > 1023)  //clock counter大於1023（不是起始綠燈）且 pass = 1 時，換成起始綠燈第一個cycle
 			current_state = green_light;
-			else
-				current_state = next_state;
-		end
+		else if(clock_counter == 0)
+			current_state = green_light;
+		else
+			current_state = next_state;
 	end
 	//next state logic
-	always @(current_state)                        
+	always @(clock_counter)                        
 	begin
 		if(clock_counter < 1023)
 			next_state = green_light;
@@ -70,7 +69,7 @@ module Traffic_control(
 			next_state = green_light;
 	end
 	//ouput logic
-	always @(current_state)
+	always @(clock_counter)//current_state 不會變
 	begin
 		case(current_state)
 			red_light:
@@ -107,7 +106,7 @@ module Datapath(
 	input pass,
 	output reg [11:0]clock_counter = 0
 );
-	always @(posedge clk or posedge rst == 1)
+	always @(posedge clk or posedge rst)
 	begin
 		if(rst)
 			clock_counter = 0;
